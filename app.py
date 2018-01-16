9b183a654f156b3158737f08c91db25e33fec4c1
dhmodi <dhaval.modi@hpe.com>
2018-01-16 20:03:10 +0530

    d

--- app.py
+++ app.py
@@ -31,17 +31,17 @@ socketio = SocketIO(app)
 parser = ""
 
 #### if WINDOWS
-# import os
-# currDir = os.getcwd()
-# print(currDir)
-# ORACLE_HOME = os.path.join(currDir,"lib")
-# PATH = os.environ.get('PATH')
-# os.environ['ORACLE_HOME'] = ORACLE_HOME
-# os.environ['PATH'] = ORACLE_HOME + ";" + PATH
+import os
+currDir = os.getcwd()
+print(currDir)
+ORACLE_HOME = os.path.join(currDir,"lib")
+PATH = os.environ.get('PATH')
+os.environ['ORACLE_HOME'] = ORACLE_HOME
+os.environ['PATH'] = ORACLE_HOME + ";" + PATH
 
 ### if LINUX
-os.environ['LD_LIBRARY_PATH'] = '/app/lib/'
-subprocess.call(['sh','downloadLib.sh'])
+# os.environ['LD_LIBRARY_PATH'] = '/app/lib/'
+# subprocess.call(['sh','downloadLib.sh'])
 
 dsnStr = cx_Oracle.makedsn("129.158.70.122", "1521", "ORCL")
 
@@ -85,18 +85,34 @@ def processRequest(req):
         for query in queries:
             table = query.get_from().get_table()
             columns = query.get_select().get_columns()
+            conditions = query.get_where().get_conditions()
             queryString = queryString + str(query)
 
         print(table)
         print(list(columns))
+
+        # print(conditions[1][1].get_column_type())
+
         print(queryString)
         cur = conn.cursor()
         cur.execute(queryString)
         rows = cur.fetchall()
-        outText = ', '.join(str(x) for x in rows[0])
-        outText = ', '.join(str(element).split(".")[0] for row in rows for element in row)
         count = 0
+        if len(conditions) != 0:
+            whereColumn=[]
+            whereValue =[]
+            for i in range(0, len(conditions)):
+                print(conditions[i][1].get_column().rsplit('.',1)[1].rstrip(')'))
+                print(conditions[i][1].get_value().strip("'"))
+                whereColumn.append(conditions[i][1].get_column().rsplit('.',1)[1].rstrip(')'))
 
+                if " MAX" not in conditions[i][1].get_value()and " MIN" not in conditions[i][1].get_value():
+                    whereValue.append(conditions[i][1].get_value().strip("'"))
+                else:
+                 if " MAX" in conditions[i][1].get_value():
+                     whereValue.append("max")
+                 else:
+                     whereValue.append("min")
         outText = "The "
         for row in rows:
             isLast = len(row)
@@ -110,16 +126,32 @@ def processRequest(req):
                 if (operation is None):
                     print("The Operation is None")
                     column = OutMap.get(column)
-                    outText = outText + str(column) + " is " + value
+                    whereValue1 = OutMap.get(whereValue[0]) if (OutMap.get(whereValue[0])) else whereValue[0]
+                    whereColumn1 = OutMap.get(whereColumn[0]) if (OutMap.get(whereColumn[0])) else whereColumn[0]
+                    print(whereValue[1])
+                    print(whereColumn[1])
+                    whereValue2 = OutMap.get(whereValue[1]) if (OutMap.get(whereValue[1])) else whereValue[1]
+                    whereColumn2 = OutMap.get(whereColumn[1]) if (OutMap.get(whereColumn[1])) else whereColumn[1]
+                    if 'whereColumn' in locals():
+                        outText = str(column) + " " + value + " in the " + str(whereColumn1) + " " + str(whereValue1) + " has " + str(whereValue2) + " " + str(whereColumn2)
+                    else:
+                        outText = outText + str(column) + " is " + value
                 elif (operation is "COUNT"):
                     table = OutMap.get(table)
                     print("The Operation is " + str(operation))
-                    outText = outText + str(operation).lower() + " of " + str(table) + " is " + value
+                    if 'whereColumn' in locals():
+                        outText =  "There are " + value + " " + str(table) + " with " + str(whereValue[0]) + " " + str(whereColumn[0])
+                    else:
+                        outText =  "There are " + value + " " + str(table)
                 else:
                     #operation = OutMap.get(str(operation).lower())
                     column = OutMap.get(column)
+                    #whereValue = OutMap.get(whereValue)
                     print("The Operation is " + str(operation))
-                    outText = outText + str(operation).lower() + " of " + str(column) + " is " + value
+                    if 'whereColumn' in locals():
+                        outText = "There are " + value + " " + str(column) + " with " + str(whereValue[0]) + " " + str(whereColumn[0])
+                    else:
+                        outText = "There are " + value + " " + str(column)
                 if (isLast is not 0):
                     outText = outText + " and the "
                     count = count + 1
