f3d9d6d6e22ebbae79898c090cec9bd108f9a337
dhmodi <dhaval.modi@hpe.com>
2018-02-07 21:56:10 +0530

    d

--- app.py
+++ app.py
@@ -32,17 +32,17 @@ socketio = SocketIO(app)
 parser = ""
 
 ### if WINDOWS
-import os
-currDir = os.getcwd()
-print(currDir)
-ORACLE_HOME = os.path.join(currDir,"lib")
-PATH = os.environ.get('PATH')
-os.environ['ORACLE_HOME'] = ORACLE_HOME
-os.environ['PATH'] = ORACLE_HOME + ";" + PATH
+# import os
+# currDir = os.getcwd()
+# print(currDir)
+# ORACLE_HOME = os.path.join(currDir,"lib")
+# PATH = os.environ.get('PATH')
+# os.environ['ORACLE_HOME'] = ORACLE_HOME
+# os.environ['PATH'] = ORACLE_HOME + ";" + PATH
 
 # ### if LINUX
-# os.environ['LD_LIBRARY_PATH'] = '/app/lib/'
-# subprocess.call(['sh','downloadLib.sh'])
+os.environ['LD_LIBRARY_PATH'] = '/app/lib/'
+subprocess.call(['sh','downloadLib.sh'])
 
 dsnStr = cx_Oracle.makedsn("129.158.70.122", "1521", "ORCL")
 
